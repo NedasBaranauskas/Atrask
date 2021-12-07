a79af8341a88f85790a1948e84176bb7397ee88f
Denisas13 <d.seredochas@gmail.com>
2021-12-07 02:53:15 +0200

    Infobar structure, add marker function v1

--- js/script.js
+++ js/script.js
@@ -6,22 +6,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([54.687157, 25.279652]).addTo(map).on('click', onClick);
+   addMarker(map,54.687157, 25.279652);
+    //addMarker(map,0);
+    //L.marker([54.687157, 25.279652]).addTo(map).on('click', onClick);
 
     fetch('./../data/markers.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
         console.log(data.markers[0].name);
-        document.getElementById("testH2").innerText = data.markers[0].name;
+        document.getElementById("name1").innerText = data.markers[0].name;
     });
+
+    
 
 
 });
 
-
-
 function onClick(e) {
     // alert(e.latlng);
+    
     document.getElementById("check2").checked=true;
+  //  document.getElementById("name").innerText=name;
+  //  document.getElementById("info").innerText=info;
+  //  document.getElementById("category").innerText=category;
+  //  document.getElementById("thumbnail").src=image;
+
+}
+
+function addMarker(map,lat,lon){
+    L.marker([lat,lon]).addTo(map).on('click', onClick);
 }
